fd4781ca71762aaaee51cb6508c0eb81eaa23a22
NhanTran <nhanrooney112@gmail.com>
2022-10-28 09:19:02 +0700

    remove variable not use

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,10 +1,7 @@
 import * as React from 'react'
 
-import { styled } from '@mui/material/styles'
 import Box from '@mui/material/Box'
-import Paper from '@mui/material/Paper'
-import LogoSignUp from '@assets/images/logo-signup.png'
-import { FilledInput, FormControl, FormHelperText, Input, InputLabel, OutlinedInput, TextField } from '@mui/material'
+import { FormControl, InputLabel, OutlinedInput } from '@mui/material'
 
 interface ISignup {}
 const Signup: React.FunctionComponent<ISignup> = () => {
